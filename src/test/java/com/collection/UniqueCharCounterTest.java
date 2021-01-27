d84dfe8a3105c9f1e59f834580cbe384f9f08347
SlavaShedu <shedu.code@gmail.com>
2021-01-27 20:49:21 +0300

    added ParameterizedTest and split on two putResultInCache, getCharsFromCache

--- src/test/java/com/collection/UniqueCharCounterTest.java
+++ src/test/java/com/collection/UniqueCharCounterTest.java
@@ -6,49 +6,69 @@ import static org.mockito.Mockito.*;
 import java.util.*;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class UniqueCharCounterTest {
 
-  UniqueCharCounter counter;
+  private UniqueCharCounter counter;
+  private CacheMemory cacheMemory;
 
   @BeforeEach
   void init() {
     counter = new UniqueCharCounter();
+    cacheMemory = mock(CacheMemory.class);
   }
 
-  @Test
-  void getResultOfCounting_shouldReturnMap() {
-    Map<Character, Long> expected = new LinkedHashMap<>();
-    expected.put('a', 1L);
-    expected.put('b', 2L);
-    expected.put('c', 4L);
-    expected.put('d', 3L);
-    expected.put('e', 5L);
+  @ParameterizedTest
+  @MethodSource("resultProvider")
+  void getResultOfCounting_shouldReturnMap(String input, Map<Character, Long> expected) {
 
-    Result result = counter.getResultOfCounting("abbccccdddeeeee");
+    Result result = counter.getResultOfCounting(input);
     Map<Character, Long> actual = result.getNumberOfChars();
 
     assertEquals(expected, actual);
   }
   
+	private static Object[][] resultProvider() {
+		return new Object[][] { 
+			{ "abcde", Map.of('a', 1L, 'b', 1L, 'c', 1L, 'd', 1L, 'e', 1L) },
+			{ "4**2!+*!", Map.of('4', 1L, '*', 3L, '2', 1L, '!', 2L, '+', 1L) },
+			{ "aRtAWwW", Map.of('a', 1L, 'R', 1L, 't', 1L, 'A', 1L, 'W', 2L, 'w', 1L) },
+			{ "ab 1a@", Map.of('a', 2L, 'b', 1L, ' ', 1L, '1', 1L, '@', 1L) }, 
+			{ "   ", Map.of(' ', 3L) }
+			};
+	}
+  
   @Test
-  void getResultOfCounting_shouldPutInCacheOnce_whenTheSameInput() {
+  void getResultOfCounting_shouldPutInCacheOnce() {  
     String input = "abc";
     Map<Character, Long> chars = new LinkedHashMap<>();
     chars.put('a', 1L);
     chars.put('b', 1L);
     chars.put('c', 1L);
     
-    CacheMemory cacheMemoryMock = mock(CacheMemory.class);
-    counter = new UniqueCharCounter(cacheMemoryMock);
-    when(cacheMemoryMock.contains(input)).thenReturn(false).thenReturn(true).thenReturn(true);
+    counter = new UniqueCharCounter(cacheMemory);
+    when(cacheMemory.contains(input)).thenReturn(false);
     
     counter.getResultOfCounting(input);
-    counter.getResultOfCounting(input);
+    
+    verify(cacheMemory).contains(input);
+    verify(cacheMemory, times(1)).putResultInCache(input, chars);
+    verifyNoMoreInteractions(cacheMemory);
+  }
+  
+  @Test
+  void getResultOfCounting_shouldGetFromCacheOnce() {
+    String input = "abc"; 
+    counter = new UniqueCharCounter(cacheMemory);
+    when(cacheMemory.contains(input)).thenReturn(true);
+    
     counter.getResultOfCounting(input);
     
-    verify(cacheMemoryMock, times(1)).putResultInCache(input, chars);
-    verify(cacheMemoryMock, times(2)).getCharsFromCache(input); 
+    verify(cacheMemory).contains(input);
+    verify(cacheMemory, times(1)).getCharsFromCache(input);
+    verifyNoMoreInteractions(cacheMemory);
   }
 
   @Test
@@ -67,6 +87,5 @@ class UniqueCharCounterTest {
         () -> assertEquals(Collections.emptyMap(), result.getNumberOfChars()),
         () -> assertNull(result.getInput())
         );
-    
   }
 }
