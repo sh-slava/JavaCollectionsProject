ed69856bbb99df421237387a49417ae1fe8e4069
SlavaShedu <shedu.code@gmail.com>
2021-01-27 17:08:47 +0300

    added contains_shouldReturnBoolean_whenCacheHasOrNotConformity()

--- src/test/java/com/collection/CacheMemoryTest.java
+++ src/test/java/com/collection/CacheMemoryTest.java
@@ -1,6 +1,6 @@
 package com.collection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.*;
 
@@ -36,5 +36,21 @@ class CacheMemoryTest {
       Map<Character, Long> actual = cacheMemory.getCharsFromCache(input);
       
       assertEquals(Collections.emptyMap(), actual);
+  }
+  
+  @Test
+  void contains_shouldReturnBoolean_whenCacheHasOrNotConformity() {
+    String input = "abc";
+    Map<Character, Long> expectedChars = new LinkedHashMap<>();
+    expectedChars.put('a', 1L);
+    expectedChars.put('b', 1L);
+    expectedChars.put('c', 1L);
+    cacheMemory.putResultInCache(input, expectedChars);
+    
+    assertAll(
+        () -> assertEquals(expectedChars, cacheMemory.getCharsFromCache(input)), 
+        () -> assertEquals(true, cacheMemory.contains(input)),
+        () -> assertEquals(false, cacheMemory.contains("differentInput"))
+        );  
   }
 }
