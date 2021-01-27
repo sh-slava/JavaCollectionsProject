c0b649acc4170fd67476d1645f6b9edf53f95c23
SlavaShedu <shedu.code@gmail.com>
2021-01-27 11:34:30 +0300

    minor change in Main class

--- src/main/java/com/collection/Main.java
+++ src/main/java/com/collection/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
   public static void main(String[] args) {
 
-    String test = "hello world!";
+    String input = "hello world!";
 
-    UniqueCharCounter counter = new UniqueCharCounter();
+    CacheMemory cacheMemory = new CacheMemory();
+    UniqueCharCounter counter = new UniqueCharCounter(cacheMemory);
     Formatter formatter = new Formatter();
-    Result result = counter.getResultOfCounting(test);
+    Result result = counter.getResultOfCounting(input);
 
     System.out.println(formatter.format(result));
   }
